220fa8875f3e56936a417c55e4dddf9e1980b53b
AbLlanos <abers2341@gmail.com>
2026-01-19 09:39:26 -0500

    version1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes),
+    //1 Añadir en app.config ts provideHttpClient();
+    provideHttpClient(),
   ]
 };
